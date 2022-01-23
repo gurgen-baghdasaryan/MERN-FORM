7d60ae7c9761be3b89470fd05218fdbf69ee0888
gugohh <oohhgug@gmail.com>
2022-01-23 15:44:03 +0100

    added our routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Navegacion from './components/Navegacion';
+import CrearUsuarios from './components/CrearUsuarios';
+import ListaUsuarios from './components/ListaUsuarios';
+
 
 function App() {
   return (
     <div className="">
-      <h1 className="text-center">Hola Gente</h1>
+      <Navegacion />
+      <div className="container p-4">
+        <Routes>
+          <Route exact path="/" element={<ListaUsuarios />} />
+          <Route path="/" element={<CrearUsuarios />} />
+          <Route path="/edit/:id" element={<CrearUsuarios />} />
+        </Routes>
+      </div>
     </div>
   );
 }
