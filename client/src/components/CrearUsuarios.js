86243780dd9cdd671c914eea9323a12bf8f51430
gugohh <oohhgug@gmail.com>
2022-01-23 21:46:15 +0100

    solved bootstrao error

--- client/src/components/CrearUsuarios.js
+++ client/src/components/CrearUsuarios.js
@@ -3,56 +3,56 @@ import React from 'react'
 const CrearUsuarios = () => {
     return (
         <div className="col-md-5 offset-md-3">
-      <div className="card card-body ">
+      <div className="card card-body">
         <form>
           <h2 className="text-center">Crear Usuario</h2>
-          <div className="mb-3 col-sm-2  col-form-label col-form-label-sm">
+          <div className="mb-3">
             <label>Nombre:</label>
             <input
               type="text"
-              className="form-cotrol"
+              className="form-control"
               placeholder="introduzca su Nombre"
               required
             />
           </div>
-          <div className="mb-3 col-sm-2 col-form-label col-form-label-sm">
+          <div className="mb-3">
             <label>Apellido:</label>
             <input
               type="text"
-              className="form-cotrol"
+              className="form-control"
               placeholder="introduzca su Apellido"
               required
             />
           </div>
-          <div className="mb-3 col-sm-2 col-form-label col-form-label-sm">
+          <div className="mb-3">
             <label>Edad:</label>
             <input
               type="number"
-              className="form-cotrol"
+              className="form-control"
               placeholder="introduzca su Edad"
               required
             />
           </div>
-          <div className="mb-3 col-sm-2 col-form-label col-form-label-sm">
+          <div className="mb-3">
             <label>Telefono:</label>
             <input
               type="Number"
-              className="form-cotrol"
+              className="form-control"
               placeholder="introduzca su Tel"
               required
             />
           </div>
-          <div className="mb-3 col-sm-2 col-form-label col-form-label-sm">
+          <div className="mb-3">
             <label>Email:</label>
             <input
               type="text"
-              className="form-cotrol"
+              className="form-control"
               placeholder="introduzca su Email"
               required
             />
           </div>
 
-          <button className="btn btn-primary">Registrar Usuario</button>
+          <button className="btn btn-primary form-control">Registrar Usuario</button>
         </form>
       </div>
     </div>
