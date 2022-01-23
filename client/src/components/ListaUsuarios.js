233a1dcd30cf7621857d2c420cc6b237a46d8066
gugohh <oohhgug@gmail.com>
2022-01-23 23:35:55 +0100

    axios get worked hehehe

--- client/src/components/ListaUsuarios.js
+++ client/src/components/ListaUsuarios.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 const ListaUsuarios = () => {
+
+    const [lista, setLista] = useState([])
+
+    useEffect(() => {
+        const getUsuarios = async()=>{
+            const res = await axios.get('http://localhost:4000/api/usuarios')
+            setLista(res.data)
+        }
+        getUsuarios();
+    }, [lista])
+
   return (
-    <div className="ff">dsfafafd</div>
+    <div className="row">
+        {
+            lista.map(list => (
+                <div className="col-md-4 p-2" key={list._id}>
+                    <div className="card">
+                        <div className="card-header">
+                            <h5>Nombre: {list.nombre}</h5>
+                        </div>
+                        <div className="card-body">
+                            <p>Apellido:{list.apellido}</p>
+                            <p>Edad: {list.edad}</p>
+                            <p>Telefono: {list.telefono}</p>
+                            <p>Email: {list.email}</p>
+                        </div>
+
+                        <div className="card-footer">
+                            <button className="btn btn-danger">
+                                Eliminar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ))
+        }
+    </div>
   );
 };
 
