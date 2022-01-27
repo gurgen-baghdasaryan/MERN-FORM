1fb9dbc3133995b41d0cc1af445c7c152b33730d
gugohh <oohhgug@gmail.com>
2022-01-27 22:05:15 +0100

    commenting the app

--- server/src/models/Usuario.js
+++ server/src/models/Usuario.js
@@ -1,4 +1,4 @@
-// aqui crearemos el mmodelo para poder acceder a nuestra base de datos y tener la schema de la informacion que vamos a guardar en el API
+// Aqui crearemos el modelo para poder acceder a nuestra base de datos y tener la schema de la informacion que vamos a guardar en el API
 
 const { Schema, model } = require('mongoose')
 
@@ -11,7 +11,7 @@ const usuarioSchema = new Schema({
 },
 
 {
-    timestamps: true,
+    timestamps: true, // Se Almacena de forma  automatica el tema de cuando se creo  y cuando fue la ultima vez que se actualizo el documento
 })
 
 module.exports = model('Usuario', usuarioSchema);
