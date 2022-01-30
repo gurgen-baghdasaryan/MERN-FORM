3a84fdfb6c8d99d50f0d2f0526dac3e65c51cc6a
gugohh <oohhgug@gmail.com>
2022-01-30 16:53:38 +0100

    commenting the controller

--- server/src/controller/usuario.controller.js
+++ server/src/controller/usuario.controller.js
@@ -1,5 +1,5 @@
 // El archivo controller tendra la logica/funcion   que permitan hacer  respuesta  a cada una de las peticiones que hagamos (la peticion GET POST PUT DELETE)
-const usuarioCtrl = {}; 
+const usuarioCtrl = {};
 
 const Usuario = require("../models/Usuario"); // Importamos nuestro modelo
 
@@ -23,21 +23,24 @@ usuarioCtrl.createUsu = async (req, res) => {
   res.json({ message: "The user has been created" });
 };
 
+// Vamos a obtener un solo usuario
 usuarioCtrl.getUsuario = async (req, res) => {
   console.log(req);
   try {
-    const usuario = await Usuario.findById(req.params.id);
-    res.json(usuario);
+    const usuario = await Usuario.findById(req.params.id); // Le decimos que busque por el Id
+    res.json(usuario); // Nos devuelve a traves de un json
   } catch (err) {
     console.log(err);
   }
 };
 
+// Logica para borrar el usuario por Id, que donde encuntre el parametro Id que nos venga por el ·req vamos a encontralo y lo vamos a eliminar de nuestro modelo
 usuarioCtrl.deleteUsu = async (req, res) => {
   await Usuario.findByIdAndDelete(req.params.id);
   res.json({ message: "User has been deleted" });
 };
 
+// Aqui tenemos la peticion ·Put, lo va buscar dentro de nuestro modelo y lo va ·Actualizar
 usuarioCtrl.updatedUsu = async (req, res) => {
   const { nombre, apellido, email, telefono, edad } = req.body;
   await Usuario.findByIdAndUpdate(req.params.id, {
@@ -50,4 +53,4 @@ usuarioCtrl.updatedUsu = async (req, res) => {
   res.json({ message: "The user has been updated" });
 };
 
-module.exports = usuarioCtrl;
+module.exports = usuarioCtrl; 
