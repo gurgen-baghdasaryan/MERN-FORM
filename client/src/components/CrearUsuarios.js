2ce9edf75d646e23602fc15cda5b57aa28e76196
gugohh <oohhgug@gmail.com>
2022-01-30 18:08:46 +0100

    created the logic for the petition Put

--- client/src/components/CrearUsuarios.js
+++ client/src/components/CrearUsuarios.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
+
 const CrearUsuarios = () => {
 
 
@@ -12,8 +15,10 @@ const CrearUsuarios = () => {
         
     }
 
-    const [usuario, setUsuario] = useState(valorInicial) //El useState() devuelve un array de 2 posiciones , la primera el valor y la segunda es la forma de actualizar ese valor
+    let {id} = useParams();
 
+    const [usuario, setUsuario] = useState(valorInicial) //El useState() devuelve un array de 2 posiciones , la primera el valor y la segunda es la forma de actualizar ese valor
+    const [subId, setSubId] = useState(id)
 
     const captruarDatos = (e) => {
         const {name, value} = e.target
@@ -35,6 +40,27 @@ const CrearUsuarios = () => {
             await axios.post('http://localhost:4000/api/usuarios', newUser)
             setUsuario({...valorInicial})
     }
+
+    // Logica para hacer una petcion a la API
+
+    const obtUno = async(valorId)=>{
+      const res = await axios.get('http://localhost:4000/api/usuarios/' +valorId)
+      setUsuario({
+        nombre:res.data.nombre,
+        apellido:res.data.apellido,
+        edad:res.data.edad,
+        telefono:res.data.telefono,
+        email:res.data.email,
+      })
+    }
+
+     useEffect(() => {
+       
+       if(subId !== ''){
+         obtUno(subId)
+       }
+     }, [subId])
+
 
     return (
         <div className="col-md-5 offset-md-3">
